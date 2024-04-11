e8cc09ed0557cf9d1fe68a915a1502571ea767ca
Reilly Spitzfaden <reillypascal@gmail.com>
2024-04-11 10:48:25 -0400

    updated website on UI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -169,7 +169,7 @@ void RSBrokenMediaAudioProcessorEditor::paint (juce::Graphics& g)
     g.drawFittedText ("RS Broken Media", 25, 10, 350, 45, juce::Justification::left, 1);
     // info
     g.setFont (juce::Font("Verdana", 16.0f, juce::Font::plain));
-    g.drawFittedText ("Version 0.3.0\n reillyspitzfaden.netlify.app", getWidth() - 375, 15, 350, 45, juce::Justification::right, 2);
+    g.drawFittedText ("Version 0.3.0\n reillyspitzfaden.com", getWidth() - 375, 15, 350, 45, juce::Justification::right, 2);
     
     // panels
     // tried 93,107,128 (outlive max patch)
